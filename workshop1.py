66956c2d79b4a530f6f702c412063f930992b5cd
volkan21 <volkanerdogus@gmail.com>
2023-04-06 09:36:17 +0300

    Create workshop1.py

---
+++ workshop1.py
@@ -0,0 +1,7 @@
+kullanicilar = list()
+for k in range(10):
+    kullaniciadi = input("Lütfen kullanci adini giriniz")
+    kullanicilar.append(kullaniciadi)
+
+for kullanici in kullanicilar:
+    print(kullanici)
